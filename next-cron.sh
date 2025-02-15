12e0494cd35f1b7986ff5fe6b897d3c06a34bc71
Alejandro de Haro <alejandro.de-haro@flixbus.com>
2025-02-15 17:35:43 +0100

    Improve output

--- next-cron.sh
+++ next-cron.sh
@@ -2,8 +2,10 @@
 
 # Determine the value of now
 NOW=$(date +"%H:%M");
-echo "Now: $NOW";
 
+echo "Now: $NOW UTC";
+
+# Split the time into hours and minutes
 IFS=':' read -ra TIME <<< "$NOW";
 HOURS=${TIME[0]};
 MINUTES=${TIME[1]};
@@ -13,6 +15,9 @@ DELTA_MINUTES=$(shuf -i 15-1500 -n 1);
 PLUS_HOURS=$((DELTA_MINUTES / 60));
 PLUS_MINUTES=$((DELTA_MINUTES % 60));
 
+echo "Delta: $PLUS_HOURS:$PLUS_MINUTES"
+
+# Calculate the next time
 NEXT_HOUR=$((HOURS + PLUS_HOURS));
 NEXT_MINUTE=$((MINUTES + PLUS_MINUTES));
 
@@ -27,7 +32,8 @@ if [ $NEXT_HOUR -gt 23 ]; then
   NEXT_HOUR=$((NEXT_HOUR - 24));
 fi
 
+echo "Next: $NEXT_HOUR:$NEXT_MINUTE UTC";
+
 # Store the next time
-echo "$NEXT_HOUR:$NEXT_MINUTE";
 NEXT_HOUR="$NEXT_HOUR" NEXT_MINUTE="$NEXT_MINUTE" envsubst < next-cron.tpl.yml > .github/workflows/next-cron.yml;
 echo -n "$NEXT_HOUR:$NEXT_MINUTE" > next-cron.txt;
