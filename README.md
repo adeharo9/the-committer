56f31b42d92f4a74ff6abcb9c50eabc9b2c21a34
Alejandro de Haro <alejandro.de-haro@flixbus.com>
2025-02-15 15:40:21 +0100

    Add some output to the action

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# The Committer
+# [The Committer](https://github.com/adeharo9/the-committer)
 
 This project consists of a single scheduled GitHub action that decides when to generate a new commit next, and then
 commits that schedule to the repository so it runs at that time. This behavior repeats in an infinite loop.
 
 ## Why?
 
-Why not? In my head it seemed fun.
+Why not? It seemed fun.
 
 ## How does it work?
 
@@ -16,7 +16,7 @@ and hour in the cron using envsubst. The action itself is simple:
   - Configure git.
   - Commit the change.
   - Push it to the repository.
-- `next.cron.sh` is a simple script that:
+- `next-cron.sh` is a simple script that:
   - Generates a random delta between 15 minutes and 24 hours.
   - Adds that to the current time.
   - Makes sure that the resulting time is correctly formed.
--- next-cron.sh
+++ next-cron.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Determine the value of now
 NOW=$(date +"%H:%M");
+echo "Now: $NOW";
 
 IFS=':' read -ra TIME <<< "$NOW";
 HOURS=${TIME[0]};
 MINUTES=${TIME[1]};
 
+# Calculate a random delta between 15min and 24h
 DELTA_MINUTES=$(shuf -i 15-1500 -n 1);
 PLUS_HOURS=$((DELTA_MINUTES / 60));
 PLUS_MINUTES=$((DELTA_MINUTES % 60));
@@ -13,14 +16,18 @@ PLUS_MINUTES=$((DELTA_MINUTES % 60));
 NEXT_HOUR=$((HOURS + PLUS_HOURS));
 NEXT_MINUTE=$((MINUTES + PLUS_MINUTES));
 
+# Ensure the minutes are within bounds
 if [ $NEXT_MINUTE -gt 59 ]; then
   NEXT_MINUTE=$((NEXT_MINUTE - 60));
   NEXT_HOUR=$((NEXT_HOUR + 1));
 fi
 
+# Ensure the hours are within bounds
 if [ $NEXT_HOUR -gt 23 ]; then
   NEXT_HOUR=$((NEXT_HOUR - 24));
 fi
 
+# Store the next time
+echo "$NEXT_HOUR:$NEXT_MINUTE";
 NEXT_HOUR="$NEXT_HOUR" NEXT_MINUTE="$NEXT_MINUTE" envsubst < next-cron.tpl.yml > .github/workflows/next-cron.yml;
 echo -n "$NEXT_HOUR:$NEXT_MINUTE" > next-cron.txt;
